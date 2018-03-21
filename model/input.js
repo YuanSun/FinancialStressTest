42b2983fba0849c6f4488c4a46e88b6a17c661f7
Yuan Sun <Yuan.Sun@nakisa.com>
2018-03-21 10:25:50 -0400

    Calculate income tax

--- model/input.js
+++ model/input.js
@@ -28,8 +28,70 @@ function getIncomeTax(income, taxRates) {
         return;
     }
 
-    // Calculation not good.
-    var taxLevel = taxRateLevels.sort((a, b) => Math.abs(income - a) - Math.abs(income - b))[0];
+    var taxRate = bruteForceSearchRate(taxRateLevels, income);
+	// var taxRate = binarySearchRate(taxRateLevels, income, 0, taxRateLevels.length - 1);
+	return marginalIncomeTaxRate2017[taxRate].total * income / 100;
+   
+}
+
+function bruteForceSearchRate(taxRateLevels, income) {
+	 if (taxRateLevels === null || taxRateLevels === undefined) {
+        return;
+    }
+
+    if(taxRateLevels.length == 1) {
+        return taxRateLevels[0];
+    }
+	
+	if (income <= taxRateLevels[0]) {
+        return taxRateLevels[0];
+    }
+	
+    if (income >= taxRateLevels[taxRateLevels.length - 1]) {
+        return taxRateLevels[taxRateLevels.length - 1];
+    }
+	
+	for (i = 1; i < taxRateLevels.length - 1; i++) {
+		if (income >= taxRateLevels[i] && income < taxRateLevels[i+1]) {
+			return taxRateLevels[i];
+		}
+	}
+}
+
+function binarySearchRate(taxRateLevels, income, beginIndex, endIndex) {
+    // taxRateLevels is supposed to be an array
+
+    if (taxRateLevels === null || taxRateLevels === undefined) {
+        return;
+    }
+
+    if(taxRateLevels.length == 1) {
+        return taxRateLevels[0];
+    }
+
+    if(taxRateLevels.length == 2 
+        && income >= taxRateLevels[0] && income < taxRateLevels[1]) {
+        return taxRateLevels[0];
+    }
+
+    if (income <= taxRateLevels[beginIndex]) {
+        return taxRateLevels[beginIndex];
+    }
+    if (income >= taxRateLevels[endIndex]) {
+        return taxRateLevels[endIndex];
+    }
+
+    var midIndex = Math.floor((beginIndex + endIndex) / 2);
+    if(income > taxRateLevels[midIndex]) {
+        return binarySearchRate(taxRateLevels.slice(midIndex + 1, endIndex + 1), 
+        income, midIndex + 1, endIndex);
+    }
+
+    if(income < taxRateLevels[midIndex]) {
+        return binarySearchRate(taxRateLevels.slice(bginIndex, midIndex + 1), 
+        income, beginIndex, midIndex);
+    }
+
 }
 
 function QuebecMarginalIncomeTaxRate(federal, quebec) {
