66ee03463c7b418271a444a8e617750936f0fdd7
SunYuan <reggie.sunyuan@me.com>
2018-04-26 06:54:50 -0400

    small changes

--- public/angular-app/tax-list/tax-list-controller.js
+++ public/angular-app/tax-list/tax-list-controller.js
@@ -5,19 +5,23 @@ function TaxRateController(incomeTaxFactory){
     var currentTaxYear = '2017';
     vm.title = 'Income Tax Rate for ' + currentTaxYear;
     vm.testQuant = 1;
-
+    var rates = {};
     incomeTaxFactory.taxRate().then(function(response) {
         // console.log("Here is in the TaxRateController");
         vm.taxRate = response;
-    });
-
-    var rates = vm.taxRate;
-    var rateArr = Object.keys(rates);
-
-    d3.select('#chart')
+        rates = vm.taxRate;
+        console.log(rates);
+        var rateArr = Object.keys(rates);
+        d3.select('#chart')
         .selectAll('div')
         .data(rateArr)
         .enter()
         .append('div')
         .style("height", (d) => d + 'px');
+    });
+
+    
+    
+
+ 
 }
